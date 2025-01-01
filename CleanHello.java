9b3280876abe7d035eec3af2cda826eb8e997651
CamilleFix <camille.flieller@orange.fr>
2025-01-01 16:13:11 +0100

    First Java Commit

---
+++ CleanHello.java
@@ -0,0 +1,38 @@
+package hello;
+
+/**Ceci est une implémentation du message traditionnel "Hello world!"
+ * @author Camille
+ */
+
+public class CleanHello {
+
+   /**Le programme commence ici*/
+    public static void main(String[] args) {
+
+        int[] myArray = new int []{7,2,4};
+        sayHelloTo("world");
+        printClapYourHands(4);
+        printArray(myArray);
+
+
+    }
+
+    /** affiche le message "hello" au destinataire fourni
+     * @param recipient
+     */
+
+    private static void sayHelloTo(String recipient) {
+        System.out.println("Hello " + recipient);
+    }
+
+    private static void printClapYourHands(int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println("Clap your hands!");
+        }
+    }
+    private static void printArray(int[] array){
+            for (int i=0; i<array.length;i++){
+                System.out.println(array[i]);
+            }
+        }
+    }
